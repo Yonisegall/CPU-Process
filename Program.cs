ae8df7495c7a4c46f9a2a19c1e3ec17b749610ae
Yonisegall <yonisegall@gmail.com>
2024-06-16 18:08:10 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 1 || !int.TryParse(args[0], out int n)){
+
+            Console.WriteLine("Usage: CPU-Process <n>");
+            return;
+        }
+
+        Console.WriteLine("Running intensive calculations...");
+
+        Stopwatch runinng_time = new Stopwatch();
+
+        double result = 1.0;
+
+        runinng_time.Start();
+
+        for (int i = 1; i < n; i++)
+        {
+
+            result += Math.Pow(Math.Cos(Math.Sqrt(i) * 9999), 9);
+        }
+        runinng_time.Stop();
+
+        Console.WriteLine(result);
+
+        Console.WriteLine($"Time for {n} iterations: {runinng_time.Elapsed.TotalMilliseconds} ms");
+    }
+}
+
