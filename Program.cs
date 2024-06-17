354cf6f2a93f2cf83a63fe7e67f1f77fc0a31667
Yonisegall <yonisegall@gmail.com>
2024-06-17 11:51:32 +0300

    b - change

--- Program.cs
+++ Program.cs
@@ -5,13 +5,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        if (args.Length != 1 || !int.TryParse(args[0], out int n)){
 
-            Console.WriteLine("Usage: CPU-Process <n>");
-            return;
-        }
+        int iterations = int.Parse(args[0]);  // Number of the iterations
+
+        int concurrentInstances = int.Parse(args[1]); // Number of concurrent instances
+
+        Task<double>[] tasks = new Task<double>[concurrentInstances];
 
         Console.WriteLine("Running intensive calculations...");
+
+        for (int i = 0; i < concurrentInstances; i++){
+            tasks[i] = Task.Run(() => RunIntensiveCalculations(iterations));
+        }
+
+        Task.WaitAll(tasks);
+
+        double totalTime = 0;
+
+        for (int i = 0; i < concurrentInstances; i++){
+            totalTime += tasks[i].Result;
+        }
+
+        double averageTime = totalTime / concurrentInstances;
+
+        Console.WriteLine($"Average time for {concurrentInstances} instances: {averageTime} ms");
+    }
+
+
+
+    static double RunIntensiveCalculations(int iterations){
 
         Stopwatch runinng_time = new Stopwatch();
 
@@ -19,16 +41,24 @@ class Program
 
         runinng_time.Start();
 
-        for (int i = 1; i < n; i++)
-        {
+        for (int i = 1; i < iterations; i++){
 
-            result += Math.Pow(Math.Cos(Math.Sqrt(i) * 9999), 9);
+            result += Math.Pow(Math.Sin(Math.Pow(Math.Cos(Math.Sqrt(i) * 1999), 10)), 22) /
+                      Math.Pow(Math.Sin(Math.Pow(Math.Cos(Math.Sqrt(i) * 1999), 9)), 5);
+
+            for (int j = 1; j < iterations; j++){
+
+                result += Math.Log(Math.Sqrt(j) * result);
+            }
         }
+
         runinng_time.Stop();
 
-        Console.WriteLine(result);
+        return runinng_time.Elapsed.TotalMilliseconds;
 
-        Console.WriteLine($"Time for {n} iterations: {runinng_time.Elapsed.TotalMilliseconds} ms");
+        //Console.WriteLine(result);
+
+        //Console.WriteLine($"Time for {iterations} iterations: {runinng_time.Elapsed.TotalMilliseconds} ms");
     }
 }
 
